8cf817e72fdb478b8bbb5a55b28d9f222d7256ba
seazyns <yashpatolia1@gmail.com>
2024-12-31 15:22:59 -0500

    feat: add execute command #4

---
+++ cogs/commands/exec.py
@@ -0,0 +1,17 @@
+import discord
+from discord.ext import commands
+from discord import app_commands
+
+class Exec(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @app_commands.command(name='exec', description='executes a command')
+    @app_commands.describe(command='command to run in game')
+    async def exec(self, interaction: discord.Interaction, command: str) -> None:
+        self.client.bot.chat(f'/{command}')
+        embed = discord.Embed(colour=discord.Color.green(), description=f'**Executed:** `/{command}`')
+        await interaction.response.send_message(embed=embed)
+
+async def setup(client):
+    client.add_cog(Exec(client))
